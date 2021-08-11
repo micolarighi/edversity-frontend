2cd7435b6bdc2b29e4617e6b17f40a0834e34e48
micolarighi <81902126+micolarighi@users.noreply.github.com>
2021-08-11 14:28:44 +0700

    Delete bukakelas-pelajar.js

--- bukakelas-pelajar.js
+++
@@ -1,9 +0,0 @@
-const triggerTabList = [].slice.call(document.querySelectorAll("#myTab a"));
-triggerTabList.forEach((triggerEl) => {
-  const tabTrigger = new mdb.Tab(triggerEl);
-
-  triggerEl.addEventListener("click", (event) => {
-    event.preventDefault();
-    tabTrigger.show();
-  });
-});
